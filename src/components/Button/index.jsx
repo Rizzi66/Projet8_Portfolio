d84db1b1d1ad28d09023d2bccba360e4db615d51
Romain Segarizzi <romain.segarizzi@hotmail.fr>
2024-07-12 17:11:59 +0200

    Updates

--- src/components/Button/index.jsx
+++
@@ -1,9 +0,0 @@
-import './index.scss'
-
-function Button( {title} ) {
-  return (
-    <button className="button-48" role="button"><span className="text">{title}</span></button>
-  )
-}
-
-export default Button
